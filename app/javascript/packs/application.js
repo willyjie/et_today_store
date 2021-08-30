9fa7fd405372f6bb6ff347911485013c1a57c88d
willyjie <willy.chang@solmate.cc>
2021-08-30 17:27:29 +0800

    install Vue3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import initVueApp from "@/main.js"
+
+initVueApp()
 
 require("stylesheets/application.scss")
 
